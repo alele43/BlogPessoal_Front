8547c1dc15f30346cdfb4299c0c4c9d9fe497768
Alessandra <alessandrasousa_43@hotmail.com>
2021-09-14 09:35:09 -0300

    Deploy Frontend BlogPessoal

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './service/auth.service';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit  {
+  
+  constructor(
+    public auth: AuthService
+  ){}
+
+  ngOnInit(){
+
+  }
+  
+}
---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,54 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { User } from '../model/User';
+import { UserLogin } from '../model/UserLogin';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) { }
+
+  entrar(userLogin: UserLogin): Observable<UserLogin>{
+    return this.http.post<UserLogin>(' https://alessandrasousa.herokuapp.com/usuarios/logar', userLogin)
+  }
+
+  cadastrar(user: User): Observable<User>{
+    return this.http.post<User>(' https://alessandrasousa.herokuapp.com/usuarios/cadastrar', user)
+
+  }
+
+  getByIdUser(id: number): Observable<User>{
+    return this.http.get<User>(`https://alessandrasousa.herokuapp.com/usuarios/${id}`)
+
+  }
+
+
+  logado(){
+    let ok: boolean = false
+
+    if (environment.token != ''){
+      ok = true
+    }
+
+    return ok
+  }
+
+  adm(){
+    let ok: boolean = false
+
+    if (environment.tipo == 'adm'){
+      ok = true
+    }
+
+    return ok
+  }
+  
+}
